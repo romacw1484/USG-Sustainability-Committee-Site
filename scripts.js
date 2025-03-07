88b95d7c8cfee4a7d27eab59c7164ac5684ae425
Will Romac <131512356+romacw1484@users.noreply.github.com>
2025-03-07 13:20:43 -0500

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,6 @@
+// js/scripts.js
+
+document.addEventListener('DOMContentLoaded', function() {
+  // Future JavaScript functionality can be added here.
+  console.log("Website framework loaded.");
+});
